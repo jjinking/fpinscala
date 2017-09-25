bc6f783e939254fbd06ba3a1238d6ada632adb70
joseph.kim <joseph.kim@workday.com>
2017-09-24 19:38:48 -0700

    Finished up to 6.5

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -30,17 +30,46 @@ object RNG {
       (f(a), rng2)
     }
 
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i1, rng2) = rng.nextInt
+    if (i1 == Int.MinValue) (0, rng2)
+    else (abs(i1), rng2)
+  }
 
-  def double(rng: RNG): (Double, RNG) = ???
+  def double(rng: RNG): (Double, RNG) = {
+    val (i1, rng2) = nonNegativeInt(rng)
+    -(il.toDouble / Int.MinValue), rng2
+  }
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+  def intDouble(rng: RNG): ((Int,Double), RNG) = {
+    val (i, rng2) = rng.nextInt
+    val (d, rng3) = double(rng2)
+    ((i, d), rng3)
+  }
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
+  def doubleInt(rng: RNG): ((Double,Int), RNG) = {
+    val ((i, d), r2) = intDouble(rng)
+    ((d, i), r2)
+  }
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+  def double3(rng: RNG): ((Double,Double,Double), RNG) = {
+    val (d1, rng2) = double(rng)
+    val (d2, rng3) = double(rng2)
+    val (d3, rng4) = double(rng3)
+    (d1, d2, d3), rng4
+  }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    if (count <= 0) (Nil, rng)
+    else {
+      val (i, r1) = rng.nextInt
+      val (l, r2) = ints(count - 1)(r1)
+      (i :: l, r2)
+    }
+  }
+
+  def doubleViaMap: Rand[Double] =
+    map[Int, Double](nonNegativeInt)(i => -(i.toDouble / Int.MinValue))
 
   def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
 
